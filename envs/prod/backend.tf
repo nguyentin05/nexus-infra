4572e525575b81745a231b6ccad63630831d172c
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-07-02 21:29:45 +0700

    chore(terraform): migrate backend locking to use_lockfile

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-065320271480-ap-southeast-1-an"
-    key            = "envs/prod/terraform.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket       = "terraform-state-065320271480-ap-southeast-1-an"
+    key          = "envs/prod/terraform.tfstate"
+    region       = "ap-southeast-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
