0412441dd6e3101c55a2fe3e7afcc69b447c9aee
Nguyen Trong Tin <tin.nguyen.cs05@gmail.com>
2026-07-02 17:46:37 +0700

    feat(eks): add terraform eks module

---
+++ modules/eks/main.tf
@@ -0,0 +1,142 @@
+locals {
+  common_tags = merge(var.tags, {
+    Environment = var.environment
+    ManagedBy   = "terraform"
+    Module      = "eks"
+  })
+}
+
+resource "aws_iam_role" "cluster" {
+  name_prefix = "${var.environment}-eks-cluster-"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "main" {
+  name     = "${var.environment}-capstone-eks"
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.cluster_version
+
+  vpc_config {
+    subnet_ids              = concat(var.private_subnet_ids, var.public_subnet_ids)
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = var.public_access_cidrs
+  }
+
+  access_config {
+    authentication_mode = "API"
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.cluster_policy]
+
+  tags = local.common_tags
+}
+
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role" "system_nodegroup" {
+  name_prefix = "${var.environment}-eks-system-node-"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  tags = local.common_tags
+}
+
+resource "aws_iam_role_policy_attachment" "system_nodegroup_worker" {
+  role       = aws_iam_role.system_nodegroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "system_nodegroup_cni" {
+  role       = aws_iam_role.system_nodegroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "system_nodegroup_ecr" {
+  role       = aws_iam_role.system_nodegroup.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_eks_node_group" "system" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.environment}-system"
+  node_role_arn   = aws_iam_role.system_nodegroup.arn
+  subnet_ids      = var.private_subnet_ids
+
+  instance_types = [var.system_node_instance_type]
+  capacity_type  = "ON_DEMAND"
+
+  scaling_config {
+    desired_size = var.system_node_desired_size
+    max_size     = var.system_node_desired_size
+    min_size     = var.system_node_desired_size
+  }
+
+  labels = {
+    role = "system"
+  }
+
+  taint {
+    key    = "CriticalAddonsOnly"
+    value  = "true"
+    effect = "NO_SCHEDULE"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.system_nodegroup_worker,
+    aws_iam_role_policy_attachment.system_nodegroup_cni,
+    aws_iam_role_policy_attachment.system_nodegroup_ecr,
+  ]
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "vpc-cni"
+  depends_on   = [aws_eks_node_group.system]
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "coredns"
+  depends_on   = [aws_eks_node_group.system]
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = "kube-proxy"
+  depends_on   = [aws_eks_node_group.system]
+}
