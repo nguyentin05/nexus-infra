cb3a771c7840eed840553da7be631ad259dc4e42
Trong Tin Nguyen <tin.nguyen.cs05@gmail.com>
2026-07-09 16:52:47 +0700

    chore(eks): add EBS CSI addon (#4)

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,13 +1,13 @@
 output "cluster_name" {
-  value = aws_eks_cluster.main.name
+  value = aws_eks_cluster.this.name
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
+  value = aws_eks_cluster.this.endpoint
 }
 
 output "cluster_ca_data" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
+  value = aws_eks_cluster.this.certificate_authority[0].data
 }
 
 output "oidc_provider_arn" {
@@ -15,5 +15,5 @@ output "oidc_provider_arn" {
 }
 
 output "oidc_provider_url" {
-  value = replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")
+  value = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
 }
